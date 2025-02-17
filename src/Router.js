834dc71c0a5a01e99d2e939454b3f781cfe196a1
MLaranjeiro <matthewlaranjeiro@hotmail.ca>
2025-02-17 11:17:57 -0500

    Fix import paths for view components in Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import View1 from './views/View1';
-import View2 from './views/View2';
-import View3 from './views/View3';
+import View1 from './views/view1';
+import View2 from './views/view2';
+import View3 from './views/view3';
 
 export default function AppRouter() {
   return (
