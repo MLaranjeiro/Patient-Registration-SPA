341bf967414bf89128996dc35492d58600ccbb9c
MLaranjeiro <matthewlaranjeiro@hotmail.ca>
2025-02-17 22:58:40 -0500

    Refactor styles: replace index.css with styles.css for improved organization and maintainability

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles.css';
 import AppRouter from './Router'; // Import the Router component
 import reportWebVitals from './reportWebVitals';
 
