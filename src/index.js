3109bb1d1f89d79fe07812ae5f44b9cac18f6229
MLaranjeiro <matthewlaranjeiro@hotmail.ca>
2025-02-17 23:15:25 -0500

    Add footer logo image and styles for improved branding

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './styles.css';
 import AppRouter from './Router'; // Import the Router component
 import reportWebVitals from './reportWebVitals';
+import logo from './images/hhsclogo.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppRouter/>   
+    <AppRouter/>
+    <div className="footer-image">
+      <img src={logo} alt="Logo" />
+    </div>   
   </React.StrictMode>
 );
 
